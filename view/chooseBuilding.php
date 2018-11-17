b682bb5f020cd7f4a6e44b6e3933544322358e4c
ibnibrahim <aliouibnibrahim@gmail.com>
2018-11-17 23:28:29 +0000

    Choose building view

---
+++ view/chooseBuilding.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="chooseBuilding.css">
+        <title>Choose Building</title>
+    </head>
+    <body>
+        <div id="mainWrapper">
+            <div id="title">
+                <h1>And codifier</h1>
+            </div>
+            <div id="buildingList">
+                <p> <a href="#"> Pavillon A </a> </p>
+                <p> <a href="#"> Pavillon B </a> </p>
+                <p> <a href="#"> Pavillon C </a> </p>
+                <p> <a href="#"> Pavillon F </a> </p>
+            </div>
+        </div>
+    </body>
+</html>
