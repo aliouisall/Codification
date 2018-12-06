01e0f706b25bc66731b8acac778c79a89dc05a23
ibnibrahim <aliouibnibrahim@gmail.com>
2018-12-06 16:12:00 +0000

    Create Account html page and stylesheet

---
+++ view/createAccount.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Create Account</title>
+		<link rel="stylesheet" href="createAccount.css">
+	</head>
+	<body>
+		<div id="mainWrapper">
+			<div id="title">
+				<h1> Title </h1>
+			</div>
+			<div id="form">
+				<form action="">
+					<p>
+						<input type="text" name="login" id="login" placeholder="Login" required>
+					</p>
+					<p>
+						<input type="text" name="numCarte" id="numCarte" placeholder="N° Carte Étudiant" required>
+					</p>
+					<p>
+						<input type="password" name="mdp" id="mdp" placeholder="Mot de passe" required>
+					</p>
+					<p>
+						<input type="password" name="mdpConf" id="mdpConf" placeholder="Mot de passe de confirmation" required>
+					</p>
+					<p>
+						<input type="submit" value="S'inscrire">
+					</p>
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
