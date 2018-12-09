51d1c761bffb1232a62fd70c0ead4ea6cfb3c1d9
ibnibrahim <aliouibnibrahim@gmail.com>
2018-12-09 02:54:07 +0000

    Login html page and stylesheet

--- view/login.php
+++ view/login.php
@@ -4,9 +4,27 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>Login</title>
+		<title>Create Account</title>
+		<link rel="stylesheet" href="login.css">
 	</head>
 	<body>
-		
+		<div id="mainWrapper">
+			<div id="title">
+				<h1> And Codifier </h1>
+			</div>
+			<div id="form">
+				<form action="">
+					<p>
+						<input type="text" name="login" id="login" placeholder="Login" required>
+					</p>
+					<p>
+						<input type="password" name="mdp" id="mdp" placeholder="Mot de passe" required>
+					</p>
+					<p>
+						<input type="submit" value="Se connecter">
+					</p>
+				</form>
+			</div>
+		</div>
 	</body>
 </html>
