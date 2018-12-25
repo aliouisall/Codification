8cf4fde6fa3c5d287df25835d88ada52a85a0d06
Mamadou Diop <diopmamadou706@gmail.com>
2018-12-25 22:19:37 +0000

    add listFile and listStudents

---
+++ listEtudiant.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>list of students</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="listEtudiant.css">
+	</head>
+	<body>
+		<div id="mainWrapper">
+			<div id="title">
+				<h1> Aand Codifier </h1>
+			</div>
+			
+				<a href="#"><span>1</span> Momo</a><br>
+				<a href="#"><span>1</span> Momo</a><br>
+				<a href="#"><span>1</span> Momo</a><br>
+				<a href="#"><span>1</span> Momo</a><br>
+				<a href="#"><span>1</span> Momo</a><br>
+				<a href="#"><span>1</span> Momo</a><br>
+
+			</div>
+		</div>
+
+
+	</body>
+</html>
---
+++ listFile.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>List of Files</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="listFile.css">
+	</head>
+	<body>
+		<div id="main">
+		<div>
+			 <div id="Div"></div>
+      		<div id="Div1"></div>
+		</div>
+		<div>
+			 <div id="Div2" ></div>
+      		<div id="Div3" ></div>
+		</div>
+		</div>
+	</body>
+</html>
